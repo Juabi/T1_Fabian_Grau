6004c3fec37b2f9b379f94f7daa19bc0321c3ac3
Fabian Grau <fabiangrau2007@gmail.com>
2026-09-21 20:40:07 -0500

    primer commit

---
+++ T1/src/t1/Alumno.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author Fabia
+ */
+public class Alumno {
+    private String nombreCompleto;
+    private String dni;
+    private String residenciaTemporal;
+    private char nivelSocioeconomico;
+    private char tipoBeca;
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getResidenciaTemporal() {
+        return residenciaTemporal;
+    }
+
+    public void setResidenciaTemporal(String residenciaTemporal) {
+        this.residenciaTemporal = residenciaTemporal;
+    }
+
+    public char getNivelSocioeconomico() {
+        return nivelSocioeconomico;
+    }
+
+    public void setNivelSocioeconomico(char nivelSocioeconomico) {
+        this.nivelSocioeconomico = nivelSocioeconomico;
+    }
+
+    public char getTipoBeca() {
+        return tipoBeca;
+    }
+
+    public void setTipoBeca(char tipoBeca) {
+        this.tipoBeca = tipoBeca;
+    }
+    
+    void verAlumno()
+    {
+        System.out.println("Alumno: Nombre: "+ this.nombreCompleto+ 
+                "Dni:" + this.dni+
+                "Residencia Temporal: "+this.residenciaTemporal +
+                "Nivel Socioeconomico" + this.nivelSocioeconomico+
+                "Beca:"+this.tipoBeca);
+    }
+}
+
---
+++ T1/src/t1/AlumnoControler.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author Fabia
+ */
+
+public class AlumnoControler {
+    
+}
